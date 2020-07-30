78b707ae5e03f07c75a6ac67f55ec9f4e2de8e6a
João Neto <joaoneto.20fsa@gmail.com>
2020-07-30 01:20:32 -0300

    Inicio da aula 3

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import PageDefault from '../../../components/PageRoot';
 
 function CadastroCategoria(){
+    const [nomeCategoria, setNomeCategoria] = useState("Filmes");
+
+    function handleInputValue(event){
+        setNomeCategoria(event.target.value);
+    }
+
     return (
         <PageDefault>
-            <h1>Cadastro de categoria</h1> 
-
+            <h1>Cadastro de categoria: {nomeCategoria}</h1> 
 
             <form>
                 <label>
                     Nome da categoria:
                     <input 
                         type="text"
+                        onChange={handleInputValue}
                     />
                 </label>
 
